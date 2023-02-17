6f15e7f401731cb630779943345a79e766b73ec3
dibkb <dibas9110@gmail.con>
2023-02-17 15:16:34 +0530

    hover navbar

--- client/src/components/InnerNav.jsx
+++ client/src/components/InnerNav.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 export const InnerNav = () => {
   return (
-    <div className="flex gap-2 items-center border border-gray-100 rounded-full py-3 px-3 text-sm font-medium">
-      <span className="border-r border-gray-300 pr-2 select-none cursor-pointer">
+    <div className="flex gap-2 items-center border border-gray-100 rounded-full py-3 px-3 text-sm font-medium cursor-pointer hover:bg-gray-100">
+      <span className="border-r border-gray-300 pr-2 select-none">
         Anywhere
       </span>
-      <span className="border-r border-gray-300 pr-2 select-none cursor-pointer">
+      <span className="border-r border-gray-300 pr-2 select-none">
         Any week
       </span>
       <small></small>
